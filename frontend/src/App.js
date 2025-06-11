d8b9281d2ee15ecbaeff2e80d0edd99b9206551d
SRET <sret.edu.in>
2025-06-11 09:18:26 +0530

    added files

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css'; // imports your CSS
+import Home from './components/home/Home.js'; // imports your Home component
+import Login from './components/pages/Login.js'; // make sure you have these files
+import Register from './components/pages/Register.js';
+import Dashboard from './components/pages/Dashboard.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<h1>404 page not found</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );g
+}
+
+export default App;
---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResponsiveAppBar from '../navbar/Navbar';
+
+function Home() {
+    return (
+        <div>
+            <ResponsiveAppBar>
+                
+            </ResponsiveAppBar>
+        </div>
+    );
+}
+
+export default Home;
